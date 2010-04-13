9dff485735fe06be9b0a2a6de73dc38742f20242
bipsandbytes <bipsandbytes@91921489-e3c1-b74d-7d1f-81519dcf28e0>
2010-04-13 06:23:23 +0000

    Trigram model DeletedInterpolationEstimator. Parameterized on lambda1 and lambda2

---
+++ trunk/cs224n/langmodel/DeletedInterpolationTrigramEstimator.java
@@ -0,0 +1,118 @@
+package cs224n.langmodel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DeletedInterpolationTrigramEstimator implements NgramProbabilityEstimator	{
+	
+	private NgramCounter ngc;
+	private NgramProbabilityEstimator baseEstimator;
+	
+	public DeletedInterpolationTrigramEstimator(NgramCounter n, NgramProbabilityEstimator baseEst)	{
+		ngc = n;
+		baseEstimator = baseEst;
+	}
+	/**
+	 * Return the Maximum Likelihood estimate of the ngram.
+	 * Since we're not doing any smoothing, we can directly
+	 * use the counts of the lower-order ngrams.
+	 */
+	public double getNgramJointProbability(List<String> ngram){
+		
+		// todo: figure out the best way to do this
+		// Here, I invoke the chain rule to calculate
+		// the joint probability
+		if (ngram.size() == 1){
+			return getNgramJointProbability(ngram);
+		}
+		if (ngram.size() == 2){
+			return getNgramJointProbability(ngram);
+		}
+		else	{
+			List<String> prevWords = new ArrayList<String>(ngram);
+			prevWords.remove(ngram.size()-1); // remove last word
+			
+			// p(ngram) = p(last-word|prevwords) * p(prevwords)
+			return getNgramConditionalProbability(ngram) * getNgramJointProbability(prevWords);
+			
+		}
+		
+	}
+	
+	public double getNgramConditionalProbability(List<String> ngram)	{
+		
+		double lambda1 = 0.8;
+		double lambda2 = 0.1;
+		double lambda3 = 1 - (lambda1+lambda2);
+	
+		if (ngram.size() == 1){
+			return baseEstimator.getNgramConditionalProbability(ngram);
+		}
+		else if (ngram.size() == 2){
+			return baseEstimator.getNgramConditionalProbability(ngram);
+		}
+		else	{
+			ArrayList<String> n_minus_1_gram = new ArrayList(ngram);
+			n_minus_1_gram.remove(0);
+			
+			ArrayList<String> n_minus_2_gram = new ArrayList(n_minus_1_gram);
+			n_minus_2_gram.remove(0);
+			
+			return 	lambda1 * baseEstimator.getNgramConditionalProbability(ngram) +
+					lambda2 * baseEstimator.getNgramConditionalProbability(n_minus_1_gram) +
+					lambda3 * baseEstimator.getNgramConditionalProbability(n_minus_2_gram);
+		}
+	}
+		
+	public double estimatorCheckModel(int order, String distribution)	{
+		return checkModelUsingConditionalProbability(order);
+//		if ("conditional".equals(distribution))	{
+//			return checkModelUsingConditionalProbability(order);
+//		}
+//		else	{
+//			return checkModelUsingJointProbability(order);
+//		}
+	}
+	
+	private double checkModelUsingJointProbability(int o) {
+        
+		double sum = 0.0;
+		LanguageSpace ls = new LanguageSpace(ngc.vocabulary, o);
+		while (ls.hasMore())	{
+			List<String> ngram = ls.getNext();
+			sum += getNgramJointProbability(ngram);
+		}
+		
+		return sum;
+		
+		/*
+		// second version : doesn't use all combinations, so goes faster
+		return estimator.checkModel();
+		//
+		*/
+	}
+	
+	/**
+	 * Sums over every conditional probability we can create.
+	 * This should equal the number of tokens we've seen.
+	 * The language-space object returns every possible set of tokens we
+	 * might encounter, including a special UNK token, which takes care
+	 * of novel words that we might encounter.
+	 */
+	private double checkModelUsingConditionalProbability(int o)	{
+		double sum = 0.0;
+		int n = 0;
+		LanguageSpace ls = new LanguageSpace(ngc.vocabulary, o);
+		while (ls.hasMore())	{
+			List<String> ngram = ls.getNext();
+			sum += getNgramConditionalProbability(ngram);
+			if (getNgramConditionalProbability(ngram) > 0.00001){
+				++n;
+			}
+		}
+		
+		return sum / (n / ngc.vocabulary.size());
+		
+	}
+
+}
